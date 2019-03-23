970bc429f6b9d2088a1b119ad52c9fdf34281490
akshay kumar <akshaykkuamer090@gmail.com>
2019-03-23 18:19:36 +0530

    another commit

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,8 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 
+# Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -17,4 +17,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
